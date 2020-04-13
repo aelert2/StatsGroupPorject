b063f78661f67bdce2c08986194f83b87fc5b3d7
aelert2 <aelert2@wisc.edu>
2020-04-13 13:17:55 -0500

    Changed file path

---
+++ FinalProject.R
@@ -0,0 +1,6 @@
+library(data.table)
+library(tidyverse)
+
+rm(list = ls())
+#nfl_data = read.csv("nflplaybyplay2009to2016/NFLPlaybyPlay 2009-2018.csv")
+nfl_data = read_csv("NFLPlaybyPlay 2009-2018.csv")
